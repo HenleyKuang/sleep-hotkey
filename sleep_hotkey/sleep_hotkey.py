4f8850f7271acf9645a68193fca4859468666e30
Henley Kuang <hkuang@brightedge.com>
2019-08-27 12:19:00 -0700

    Performance improvement - 250 milliseconds improvement in sleeping actual passed value

--- sleep_hotkey/sleep_hotkey.py
+++ sleep_hotkey/sleep_hotkey.py
@@ -1,4 +1,5 @@
 import keyboard
+import math
 import time
 
 DEFAULT_LOOP_INTERVAL = 2
@@ -6,10 +7,9 @@ DEFAULT_EXIT_KEY = 'esc'
 
 
 def sleep(seconds, loop_interval=DEFAULT_LOOP_INTERVAL, exit_key=DEFAULT_EXIT_KEY):
-    while not keyboard.is_pressed(exit_key):
-        if seconds >= loop_interval:
-            time.sleep(loop_interval)
-            seconds -= loop_interval
-        else:
-            time.sleep(seconds)
-            break
+    remaining_seconds = seconds % DEFAULT_LOOP_INTERVAL
+    count = math.floor(seconds/loop_interval)
+    while not keyboard.is_pressed(exit_key) and count > 0:
+        time.sleep(loop_interval)
+        count -= 1
+    time.sleep(remaining_seconds)
