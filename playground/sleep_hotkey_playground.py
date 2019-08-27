0d99d5b7e85518531ad74b215e618968ee2f412a
Henley Kuang <hkuang@brightedge.com>
2019-08-26 22:34:30 -0700

    Playground - Add argparse for sleep seconds

--- playground/sleep_hotkey_playground.py
+++ playground/sleep_hotkey_playground.py
@@ -1,5 +1,24 @@
+import argparse
+
 from sleep_hotkey.sleep_hotkey import sleep
 
 
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--seconds',
+                        action='store',
+                        type=int,
+                        default=10)
+    return parser.parse_args()
+
+
+def _main():
+    options = _parse_args()
+    sleep_seconds = options.seconds
+
+    sleep(sleep_seconds)
+
+
 if __name__ == "__main__":
-    sleep(10)
+    _main()
